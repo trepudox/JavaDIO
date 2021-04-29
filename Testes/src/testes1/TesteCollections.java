845b0ca961d7ceb2bea7c47f47bea5fb3538c639
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-04-28 22:33:11 -0300

    teste

---
+++ Testes/src/testes1/TesteCollections.java
@@ -0,0 +1,17 @@
+package testes1;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TesteCollections {
+    public static void main(String[] args) {
+        
+        List<Integer> lista = List.of(1, 3, 4);
+
+        lista.add(2);
+
+        System.out.println(lista);
+
+    }
+}

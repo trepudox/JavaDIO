b2060f08386b720c8ca37320ef1dabdc0d0dbc20
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-04-29 19:35:31 -0300

    optional

--- Testes/src/optionalEStream/AppOptional.java
+++ Testes/src/optionalEStream/AppOptional.java
@@ -5,31 +5,36 @@ import java.util.Optional;
 
 public class AppOptional {
     public static void main(String[] args) {
-        System.out.println();
-
         Optional<String> teste = Optional.empty();
         teste = Optional.ofNullable("tem valor aqui!!!!!");
 
-        System.out.println(teste);
+        System.out.println("\n" + teste);
         System.out.println(teste.get());
 
-        teste.ifPresent(resp -> {
+        teste.ifPresentOrElse((resp) -> System.out.println("VALOR: " + resp) , () -> System.out.println("está nulo..."));
+        teste.ifPresent((resp) -> {
             resp = "dentro do ifPresent!";
-            System.out.println(resp);
-            System.out.println();
+            System.out.println(resp + "\n");
         });
 
+        //o método orElse retorna o valor do objeto, caso o mesmo seja nulo, retorna o parametro passado
         teste = Optional.empty();
         System.out.println(teste.orElse("vazio..."));
-
+        
         System.out.println("teste.isEmpty() ? " + teste.isEmpty());
         System.out.println("teste.isPresent() ? " + teste.isPresent());
+        
+        //os dois retornam Optional.empty
+        teste = Optional.empty();
+        System.out.println(teste);
+        teste = Optional.ofNullable(null);
+        System.out.println(teste);
 
         try {
             // retorna o valor, caso o mesmo seja nulo lança essa exceção
             teste.orElseThrow();
         } catch (NoSuchElementException e) {
-            System.out.println("\nops");
+            System.out.println("\nops deu erro");
         }
 
     }

6306a66e7f5acbcc2d0b062051c393ccac7713a1
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-05-15 10:27:34 -0300

    teste

--- springboot/src/main/java/com/digitalinnovationone/springboot/controller/HelloController.java
+++ springboot/src/main/java/com/digitalinnovationone/springboot/controller/HelloController.java
@@ -1,15 +1,17 @@
 package com.digitalinnovationone.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@PropertySource("classpath:res.properties") // escolhe de onde o @Value vai tirar os valores
 public class HelloController {
 
-    @Value("Perfil ativo: ${spring.profiles.active:N/A}. Olá página vazia!")
+    @Value("Perfil ativo: ${spring.profiles.active:N/A}. Olá página vazia!") // @Value insere o valor na variavel
     private String appMessage;
 
     @GetMapping

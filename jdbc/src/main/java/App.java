211da2148fbac4fb28616cae599e4d64ea5d3c59
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-05-16 11:16:00 -0300

    criando a conexao com o bd

--- jdbc/src/main/java/App.java
+++ jdbc/src/main/java/App.java
@@ -1,5 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String url = "jdbc:mysql://localhost:3306/dio_teste";
+
+        try (Connection connection = DriverManager.getConnection(url, "root", "root")) {
+            System.out.println("Conectou");
+        } catch(SQLException e) {
+            System.out.println(e.getStackTrace());
+        }
+
+
+    //    System.out.println("Hello World!");
     }
 }

af216ae1d2f022b2c63991d32ea6ffe6f365951f
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-05-18 11:16:02 -0300

    testes

--- jdbcEjpa/src/com/trepudox/jdbc/SQL/pessoa_teste.sql
+++ jdbcEjpa/src/com/trepudox/jdbc/SQL/pessoa_teste.sql
@@ -4,16 +4,17 @@ CREATE DATABASE dio_teste;
  USE dio_teste;
  
  CREATE TABLE pessoa(
-	id INTEGER NOT NULL PRIMARY KEY,
+	id INTEGER NOT NULL AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     dataNasc DATE NOT NULL,
-    cidade VARCHAR(255) NOT NULL
+    cidade VARCHAR(255) NOT NULL,
+	PRIMARY KEY(id)
  );
  
- INSERT INTO pessoa(id, nome, dataNasc, cidade)
- VALUES (1, "Marco", "2002-07-01", "São Paulo"), (2, "Alex", "1995-11-25", "São Paulo"),
- (3, "Jorge", "1998-02-13", "Rio de Janeiro"), (4, "Beatriz", "2000-08-04", "São Paulo"),
- (5, "Maria", "1999-09-20", "Rio de Janeiro"), (6, "Carol", "2001-03-06", "Florianópolis");
+ INSERT INTO pessoa(nome, dataNasc, cidade)
+ VALUES ("Marco", "2002-07-01", "São Paulo"), ("Alex", "1995-11-25", "São Paulo"),
+ ("Jorge", "1998-02-13", "Rio de Janeiro"), ("Beatriz", "2000-08-04", "São Paulo"),
+ ("Maria", "1999-09-20", "Rio de Janeiro"), ("Carol", "2001-03-06", "Florianópolis");
  
  SELECT * FROM pessoa;
  
--- jdbcEjpa/src/com/trepudox/jdbc/teste/Teste.java
+++ jdbcEjpa/src/com/trepudox/jdbc/teste/Teste.java
@@ -1,7 +1,28 @@
-import java.sql.DriverManager;
+package com.trepudox.jdbc.teste;
+
+import java.sql.*;
+import java.time.LocalDate;
 
 public class Teste {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/dio_teste?serverTimezone=UTC";
+        String user = "root";
+        String password = "root";
+
         Connection connection = DriverManager.getConnection(url, user, password);
+
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM pessoa;");
+
+        ResultSet rs = stmt.executeQuery();
+
+        while(rs.next()) {
+            System.out.print(rs.getLong("id") + " ");
+            System.out.print(rs.getString("nome") + " ");
+            System.out.print(rs.getObject("dataNasc", LocalDate.class) + " ");
+            System.out.println(rs.getString("cidade"));
+        }
+
+
+
     }
 }

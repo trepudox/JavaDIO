55a81f482d1267eb1a82713bca222a0b6e4c1737
Marco Aurélio Queiroz <teoaa2@gmail.com>
2021-05-20 20:10:16 -0300

    commit inicial

---
+++ mercadinho/src/test/java/com/trepudox/mercadinho/model/CategoriaModelTest.java
@@ -0,0 +1,12 @@
+package com.trepudox.mercadinho.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CategoriaModelTest {
+
+//    @DisplayName("")
+//    @Test
+
+
+}
